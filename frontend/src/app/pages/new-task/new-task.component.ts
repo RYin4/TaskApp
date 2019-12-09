e0146b2bd0d5d26be32cfa70fb94ff5ee0dcfae5
Randy Yin <randyyin28@gmail.com>
2019-12-08 17:12:39 -0800

    create new list

---
+++ frontend/src/app/pages/new-task/new-task.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrls: ['./new-task.component.scss']
+})
+export class NewTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  createTask(titie: string) {
+    
+
+  }
+
+}
